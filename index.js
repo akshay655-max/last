9f7fa16390ad3f8d42fee048dda9be78d18c0f28
akshay <akshaysuman129@gmail.com>
2023-01-23 01:22:31 +0530

    Add

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongoose.connect(
 );
 
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+
+app.listen(8080, () => {
+  console.log("Server is running on port");
 });
